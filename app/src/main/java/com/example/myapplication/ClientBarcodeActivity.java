b8ee0d34195b44fa567ccb17492ffce305642a87
talhashahid <stalha872@gmail.com>
2024-01-25 18:33:22 +0500

    xyz

--- app/src/main/java/com/example/myapplication/ClientBarcodeActivity.java
+++ app/src/main/java/com/example/myapplication/ClientBarcodeActivity.java
@@ -31,8 +31,6 @@ public class ClientBarcodeActivity extends Activity implements BarcodeReader.Bar
         setContentView(R.layout.activity_barcode);
 
         offBtn=findViewById(R.id.offBtn);
-
-
         onBtn=findViewById(R.id.onBtn);
 
 
